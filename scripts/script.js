8f531580e04882f1ec58c67b056fa8f3074c8d49
sspopkov <seregapopkov98@yandex.ru>
2019-07-12 16:45:14 +0300

    Changed information

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
 var time = 0;
 var running = 0;
-console.log("dec");
 
 function start() {
-	console.log("start");
 	if (running == 0)
 		increment();
 	running = 1;
@@ -13,9 +11,16 @@ function increment() {
 	setTimeout(function() {
 		time++;
 		var minutes = Math.floor(time/10/60);
-		var seconds = Math.floor(time/10);
+		var seconds = Math.floor(time/10 % 60);
+
+		if (minutes < 10) {
+			minutes = "0" + minutes;
+		}
+		if (seconds < 10) {
+			seconds = "0" + seconds;
+		}
+
 		document.getElementById("output").innerHTML = minutes + " мин. " + seconds + " сек.";
 		increment();
-		console.log("inc");
 	}, 100);
 }
